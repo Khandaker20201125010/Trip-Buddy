bf1ac26dd8804ea356be9833334a23bfd8138147
unknown <prantokih42@gmail.com>
2025-11-30 22:23:36 +0600

    first commit

---
+++ dist/app.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
+const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
+const config_1 = __importDefault(require("./config"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+//parser
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.send({
+        message: "Server is running..",
+        environment: config_1.default.node_env,
+        uptime: process.uptime().toFixed(2) + " sec",
+        timeStamp: new Date().toISOString()
+    });
+});
+app.use(globalErrorHandler_1.default);
+app.use(notFound_1.default);
+exports.default = app;
